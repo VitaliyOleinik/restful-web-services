a8ff62fe0c0a58e3be507991d856f3cc7a2d77fa
VitaliyOleinik <oleynik624@gmail.com>
2023-09-05 22:09:02 +0600

    add h2 connection and use jpa repository

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,21 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+
+    protected User() {}
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "must be more than 2 characters")
@@ -16,9 +25,6 @@ public class User {
     @Past(message = "should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
-
-    public User() {
-    }
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
